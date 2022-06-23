61711d911201710df5ebcfd0a88f46897083f848
carlo-ddm <carloduminuco90@gmail.com>
2022-06-23 17:03:37 +0200

    creazione delle rotte nav

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::get('/personaggi', function () {
+    return view('characters');
+})->name('characters');
+
+Route::get('/fumetti', function () {
+    return view('comics');
+})->name('comics');
+
+Route::get('/films', function () {
+    return view('movies');
+})->name('movies');
+
+Route::get('/tv', function () {
+    return view('tv');
+})->name('tv');
+
+Route::get('/giochi', function () {
+    return view('games');
+})->name('games');
+
+Route::get('/collezioni', function () {
+    return view('collectibles');
+})->name('collectibles ');
+
+Route::get('/video', function () {
+    return view('videos');
+})->name('videos ');
+
+Route::get('/fans', function () {
+    return view('fans');
+})->name('fans ');
+
+Route::get('/novità', function () {
+    return view('news');
+})->name('news ');
+
+Route::get('/shop', function () {
+    return view('shop');
+})->name('shop');
